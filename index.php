fe23613bc47306e8f4ee6fd68216faf0f66c9085
Keith Chapman <keithcchapman@gmail.com>
2014-06-02 11:01:52 -0400

    1st Push

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Keith Chapman</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<div class="page1">
+		<h2>Welcome</h2>
+		<h4>To Keith Chapman's site</h4>
+	</div>
+</body>
+</html>>
+<html>
